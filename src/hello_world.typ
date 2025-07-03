d5330437e7a4dacd8c677fcdd778627052b15a7d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-07-03 22:07:27 +0300

    chore(debug): Add wasm string function

--- src/hello_world.typ
+++ src/hello_world.typ
@@ -1,3 +1,5 @@
-#import plugin("/tools/tools.wasm"): hello_world
+#import plugin("/tools/tools.wasm"): hello_string, hello_world
+#import "/src/utils/call_wasm.typ": call_wasm
 
 #str(hello_world())
+#call_wasm(hello_string, "Hello world")
--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -1,3 +1,4 @@
+use ciborium::{from_reader, into_writer};
 use wasm_minimal_protocol::*;
 
 pub mod config;
@@ -10,4 +11,23 @@ initiate_protocol!();
 #[wasm_func]
 pub fn hello_world() -> Vec<u8> {
     b"Hello, world!".to_vec()
+}
+
+#[wasm_func]
+pub fn hello_string(arg: &[u8]) -> Vec<u8> {
+    println!("{:?}", arg);
+
+    let data: String = match from_reader(arg) {
+        Ok(items) => items,
+        Err(e) => {
+            eprintln!("{:?}", e);
+            return b"Error".to_vec();
+        }
+    };
+
+    let mut output = Vec::new();
+
+    into_writer(&data, &mut output).unwrap();
+
+    output
 }
